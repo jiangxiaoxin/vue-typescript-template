6a339a21b4182258f549baba93332ae3ab94e092
Robert Kleger <robert.kleger@swisscom.com>
2018-04-23 19:29:03 +0200

    🚧  add nps, nps-utils and package-scripts.js file

---
+++ template/package-scripts.js
@@ -0,0 +1,28 @@
+/**
+ * Windows: Please do not use trailing comma as windows will fail with token error
+ */
+module.exports = {
+  scripts: {
+    default: 'webpack',
+    webpack: {
+      default: 'webpack.dev',
+      dev: 'webpack-dev-server --inline --progress --config build/webpack.dev.conf.js'
+    }
+    {{#if_or unit e2e}},
+    test: {
+      {{#if_eq runner "jest"}}
+      unit: 'jest --config test/unit/jest.conf.js --coverage',
+      {{/if_eq}}
+      {{#if_eq runner "karma"}}
+      unit: '"cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run'
+      {{/if_eq}}
+      {#e2e}}
+      e2e: 'node test/e2e/runner.js'
+      {{/e2e}}
+    }
+    {{#lint}}
+    , lint: 'eslint --ext .js,.vue src{{#unit}} test/unit{{/unit}}{{#e2e}} test/e2e/specs{{/e2e}}'
+    {{/if_or}}
+    ,build: 'node build/build.js'
+  }
+};
