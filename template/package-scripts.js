644016dcff009b5b7ecf7332dd2da840f2512022
Robert Kleger <robert.kleger@swisscom.com>
2018-04-23 19:41:05 +0200

    ✨ add nps support

--- template/package-scripts.js
+++ template/package-scripts.js
@@ -7,22 +7,23 @@ module.exports = {
     webpack: {
       default: 'webpack.dev',
       dev: 'webpack-dev-server --inline --progress --config build/webpack.dev.conf.js'
-    }
-    {{#if_or unit e2e}},
+    },
+    {{#if_or unit e2e}}
     test: {
       {{#if_eq runner "jest"}}
-      unit: 'jest --config test/unit/jest.conf.js --coverage',
+      unit: 'jest --config test/unit/jest.conf.js --coverage'{{#e2e}},{{/e2e}}
       {{/if_eq}}
       {{#if_eq runner "karma"}}
-      unit: '"cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run'
+      unit: '"cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run'{{#e2e}},{{/e2e}}
       {{/if_eq}}
-      {#e2e}}
+      {{#e2e}}
       e2e: 'node test/e2e/runner.js'
       {{/e2e}}
-    }
-    {{#lint}}
-    , lint: 'eslint --ext .js,.vue src{{#unit}} test/unit{{/unit}}{{#e2e}} test/e2e/specs{{/e2e}}'
+    },
     {{/if_or}}
-    ,build: 'node build/build.js'
+    {{#lint}}
+    lint: 'eslint --ext .js,.vue src{{#unit}} test/unit{{/unit}}{{#e2e}} test/e2e/specs{{/e2e}}',
+    {{/lint}}
+    build: 'node build/build.js'
   }
 };
