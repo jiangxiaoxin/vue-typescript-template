7b32a4e2fadff0e65bd497aa8b89f59a5b54a738
Robert Kleger <robert.kleger@swisscom.com>
2018-04-23 19:53:47 +0200

    🚧  update package-scripts.js

--- template/package-scripts.js
+++ template/package-scripts.js
@@ -9,19 +9,12 @@ module.exports = {
       dev: 'webpack-dev-server --inline --progress --config build/webpack.dev.conf.js'
     },
     test: {
-      {{#if_eq runner "jest"}}
-      unit: 'jest --config test/unit/jest.conf.js --coverage',
-      {{/if_eq}}
-      {{#if_eq runner "karma"}}
-      unit: '"cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run',
-      {{/if_eq}}
-      {{#e2e}}
-      e2e: 'node test/e2e/runner.js'
-      {{/e2e}}
-    }
-    {{#lint}}
-    lint: 'eslint --ext .js,.vue src{{#unit}} test/unit{{/unit}}{{#e2e}} test/e2e/specs{{/e2e}}',
-    {{/lint}}
+      {{#if_eq runner "jest"}}unit: 'jest --config test/unit/jest.conf.js --coverage'{{/if_eq}}
+      {{#if_eq runner "karma"}}unit: '"cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run'{{/if_eq}},{{#e2e}}
+      e2e: 'node test/e2e/runner.js'{{/e2e}}
+    }{{#lint}},
+    lint: 'eslint --ext .js,.vue src{{#unit}} test/unit{{/unit}}{{#e2e}} test/e2e/specs{{/e2e}}'
+    ,{{/lint}}
     build: 'node build/build.js'
   }
 };
